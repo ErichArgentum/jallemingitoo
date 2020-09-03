a455c206effdaced76d0ac16e8b16bb8f7fdbb00
Erich Argentum <erich.hobe@ametikool.ee>
2020-09-03 11:33:42 +0300

    ok

--- book.php
+++ book.php
@@ -1,48 +1,7 @@
 <?php 
 
+    require_once 'disconnect.php';
 
-class Book {
-
-    protected $title;
-    protected $inStock = true;
-    public $stocksaldo = 0;
-
-    public function __constrcut($t) {
-        $this->title = $t;
-
-
+    class Book {
     }
-    public function getStocksaldo() {
-        return $this->stockSaldo;
-
-
-    }
-
-    public function setStcokSaldo($v){
-    
-        if ( $v < 0 ) {
-            throw new Exception('Value should be negative!');
-        } else {
-        $this->stockSaldo = $v;
-
-    }
-    public function soldOut() {
-        $this->inStock = false;
-
-    }
-}
-
-$book =new Book('Kalevipoeg');
-$book2 = new Book('Rehepapp');
-
-
-
-$book->stockSaldo(-5);
-$book2->soldOut();
-//$book2->inStock = true;
-
-var_dump($book);
-var_dump($book2);
-var_dump($book->getStocksaldo());
-
-    echo "\n";
+$book = new Book();
